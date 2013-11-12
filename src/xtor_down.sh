0a28c448435658d37a3aeefb70ec5839dd0739dc
David MacMahon <davidm@astro.berkeley.edu>
2013-11-12 11:21:49 +0200

    Add xtor_up.sh and xtor_down.sh scripts

---
+++ src/xtor_down.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Script to totally take down the paper correlator X engines.
+#
+# Usage: xtor_down.sh [SLICES]
+#
+# SLICES is optional.  It is a list of numbers corresponding to pxN hosts.  If
+# not given, it defaults to {1..8}, i.e. all pxN hosts for PSA256.
+#
+# Examples:
+#
+#   # Shutdown px1..px8
+#   $ xtor_down.sh
+#
+#   # Shutdown px3 only
+#   $ xtor_down.sh 3
+#
+#   # Shutdown px1, px2, and px3 only
+#   $ xtor_down.sh 1 2 3
+
+# Name of PAPER server
+PAPER=paper1
+
+# Make sure we are on the PAPER server
+hostname=`hostname -s`
+if [ $hostname != $PAPER ]
+then
+  echo "This script must be run on the PAPER server (i.e. ${PAPER}), not ${hostname}"
+  exit 1
+fi
+
+# If no parameters are given (the normal case), use slices {1..8}
+if [ $# -eq 0 ]
+then
+  set {1..8}
+fi
+
+# Stop taking data
+echo -n "stopping cn_rx.py"
+pkill -INT cn_rx.py
+
+# Wait upto 20 seconds for data taking to stop
+timeout=20
+while [ $timeout -gt 0 ] && pgrep cn_rx.py > /dev/null
+do
+  echo -n .
+  timeout=$((timeout-1))
+  sleep 1
+done
+echo
+
+if [ $timeout -eq 0 ]
+then
+  echo 'timeout!'
+fi
+
+if pgrep cn_rx.py > /dev/null
+then
+  echo 'cn_rx.py still running!'
+fi
+
+# Just to be sure
+echo "killing cn_rx.py (just to be sure)"
+pkill    cn_rx.py
+pkill -9 cn_rx.py
+
+# Stop integrations
+echo "stopping integrations"
+paper_ctl.rb stop
+
+# Stop hashpipe-redis gateways
+echo "stopping hashpipe-redis gateways"
+redis-cli -h redishost publish hashpipe:///gateway quit
+
+# Just to be sure
+echo "killing any remaining hashpipe-redis gateways"
+for x; do ssh px$x pkill    -f hashpipe_redis_gateway.rb; done
+for x; do ssh px$x pkill -9 -f hashpipe_redis_gateway.rb; done
+
+# Stop hashpipe instances
+echo "killing hashpipe instances"
+for x; do ssh px$x pkill    hashpipe; done
+for x; do ssh px$x pkill -9 hashpipe; done
+
+# Delete shared memory and semaphores
+echo "deleting shared memory and semaphores"
+for x; do
+  for i in {0..3}; do
+    ssh px$x hashpipe_clean_shmem -d -I $i > /dev/null
+  done
+done
+
+# Just to be sure
+echo "nuking any remaining shared memory and semaphores"
+for x; do ssh px$x 'ipcs -m | awk "/0x/{print \"ipcrm -m\", \$2}" | sh'; done
+for x; do ssh px$x 'ipcs -s | awk "/0x/{print \"ipcrm -s\", \$2}" | sh'; done
