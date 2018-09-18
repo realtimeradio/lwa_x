3b1444cd14cf9c9e2237d286029f6dc6582d934a
jack <jack>
2018-09-18 21:17:05 +0000

    Add support for even/odd correlators

--- src/hera_ctl.py
+++ src/hera_ctl.py
@@ -4,7 +4,10 @@ import redis
 import time
 import argparse
 
-def mcnts_per_second(sample_rate=500e6, spectra_len=8192, round_to=2):
+MCNT_STEP_SIZE = 2
+MCNT_XGPU_BLOCK_SIZE = 2048
+
+def mcnts_per_second(sample_rate=500e6, spectra_len=8192):
     """
     Return number of MCNTs in 1 second. For HERA, but not in general,
     this is the same as the number of spectra in 1 second.
@@ -12,7 +15,7 @@ def mcnts_per_second(sample_rate=500e6, spectra_len=8192, round_to=2):
     spectra_len: Number of frequency channels in 1 F-Engine spectra (prior to any subselecting)
     round_to   : Output the number of mcnts per second rounded up to a multiple of this number
     """
-    return round_to * (int(sample_rate / (spectra_len * 2) + (round_to - 1)) // round_to)
+    return sample_rate / (spectra_len * 2)
 
 
 parser = argparse.ArgumentParser(description='Turn on and off the HERA correlator',
@@ -21,10 +24,14 @@ parser.add_argument('action',type=str,
                     help = 'Action: "start"|"stop": start|stop the correlator.')
 parser.add_argument('-n', dest='acclen', type=int, default=64*2048,
                     help ='Number of spectra to accumulate')
+parser.add_argument('-s', dest='slices', type=int, default=2,
+                    help ='Number of slices. Eg. for HERA\'s ever/odd correlator, slices=2. \
+                           It is assumed that for N xhosts, hosts 1..N are slice 1, \
+                           N+1..2*N are slice 2, etc.')
 parser.add_argument('-t', dest='starttime', type=float, default=time.time()+5,
                     help ='UNIX time to start observations. Default is NOW+5s')
-parser.add_argument('-x', dest='xhosts', type=int, default=16,
-                    help ='Number of GPU hosts')
+parser.add_argument('-x', dest='xhosts', type=int, default=8,
+                    help ='Number of GPU hosts **per slice**')
 parser.add_argument('-i', dest='xinstances_per_host', type=int, default=2,
                     help ='Number of X-engine instances per GPU host')
 parser.add_argument('-r', dest='redishost', type=str, default='redishost',
@@ -44,44 +51,39 @@ for xhost in range(1, args.xhosts+1):
 
 rdb = redis.Redis(args.redishost)
 
+if args.action == 'stop':
+    rdb.publish("hashpipe:///set", 'INTSTAT=stop')
+
 if args.action == 'start':
-    gpumcnts = [rdb.hget(skey, 'GPUMCNT') for skey in status_keys]
-    
-    n_missing_mcnts = gpumcnts.count(None)
-    if n_missing_mcnts == xinstances:
-        print 'Could\'t find any GPUMCNT values. Cannot start'
-        exit()
-    elif n_missing_mcnts > 0:
-        print 'WARNING: Missing GPUMCNT values from instances' % n_missing_mcnts
-
-    # try and convert these to integers. Skip on failure (probably means the value is "None")
-    for i in range(len(gpumcnts)):
-        try:
-            gpumcnts[i] = int(gpumcnts[i])
-        except:
-            pass
-
-    # Delay before starting, in MCNTs, rounded to 4096
+    # Calculate start MCNT
     mcnt_origin = int(rdb['corr:feng_sync_time'])
     time_delay = args.starttime - mcnt_origin
-    delay_mcnts = 4096 * ((int(time_delay * mcnts_per_second()) + 2047) / 4096)
+    delay_mcnts = int(time_delay * mcnts_per_second())
+    # round delay_mcnts down to an acceptable value
+    round_to = MCNT_XGPU_BLOCK_SIZE
+    trig_mcnt = delay_mcnts - (delay_mcnts % round_to)
     
-    # MCNT to start on. Don't need to round, becase gpumcnts has to be aligned to a block boundary
-    trig_mcnt = delay_mcnts
-    trig_time = delay_mcnts * mcnts_per_second() + mcnt_origin
+    trig_time = trig_mcnt / mcnts_per_second() + mcnt_origin
 
-    print 'Current MCNTs are:', gpumcnts
+    #print 'Current MCNTs are:', gpumcnts
+    print 'Sync time is %s' % time.ctime(mcnt_origin)
     print 'MCNTs per second: %.1f' % mcnts_per_second()
+    print 'Requested start time: %s' % time.ctime(args.starttime)
     print 'Trigger MCNT: %d' % trig_mcnt
-    print 'Trigger time is %.1f seconds in the future' % time_delay
+    print 'Trigger time is %.1f seconds in the future (%s)' % (trig_time - time.time(), time.ctime(trig_time))
 
     # Use the hashpipe publish channel to update keys in all status buffers.
     # See the docstring at https://github.com/david-macmahon/rb-hashpipe/blob/master/bin/hashpipe_redis_gateway.rb
     # for details about formatting
     
-    msg = 'INTSYNC=%d\nINTCOUNT=%d\nINTSTAT=start\nOUTDUMPS=0' % (trig_mcnt, args.acclen)
-    rdb.publish("hashpipe:///set", msg)
+    for slice in range(args.slices):
+        msg = 'INTSYNC=%d\nINTCOUNT=%d\nINTSTAT=start\nOUTDUMPS=0' % (trig_mcnt + slice*MCNT_STEP_SIZE, args.acclen)
+        for host in range(args.xhosts):
+            hostname = 'px%d' % (slice * args.xhosts + host + 1)
+            rdb.publish('hashpipe://%s/0/set' % hostname, msg)
+            rdb.publish('hashpipe://%s/1/set' % hostname, msg)
+        #rdb.publish("hashpipe:///set", msg)
+
     rdb['corr:trig_mcnt'] = trig_mcnt
     rdb['corr:trig_time'] = trig_time
     rdb['corr:acc_len'] = args.acclen
-
