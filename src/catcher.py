fdb9ad6eb3618b12650233a5ec7743111ac41c58
jack <jack>
2018-10-30 22:55:21 +0000

    Speed up catcher

--- src/catcher.py
+++ src/catcher.py
@@ -20,47 +20,45 @@ NPACKETS = NWORDS * 8 / PAYLOAD_LEN
 print "Expecting %d packets" % NPACKETS
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 80000000)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 800000000)
 print sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
 
 sock.bind(("10.80.40.251", 10000))
 
-dout = np.ones(2*NWORDS, dtype=np.int32) * -1
+dout = np.ones([NXENG*2, NWORDS_PER_XENG], dtype=np.int32) * -1
 
 n = 0
 wait = True
 start = time.time()
 while True:
     data = sock.recv(BYTES_PER_PACKET)
-    unpacked = struct.unpack('!QLHH', data[0:16])
-    time_demux = (unpacked[0] % 4) >> 1
-    timestamp = unpacked[0] - (unpacked[0] % 4)
+    unpacked = struct.unpack('!QLH', data[0:14])
+    #time_demux = (unpacked[0] % 4) >> 1
+    timestamp = unpacked[0] & 0xfffffffffffffffc
     xeng_id = unpacked[2]
-    offset = 4*xeng_id*NWORDS_PER_XENG + unpacked[1]
-    payload_len = unpacked[3]
+    offset = unpacked[1]
     if wait: 
         start = time.time()
         this_window = timestamp
     if (not wait) or (offset == 0):
+        if  (timestamp != this_window):
+            print timestamp, this_window
+            stop = time.time()
+            print "%d packets received in %.2f seconds" % (n, stop - start)
+            print "Missing %d packets" % (NPACKETS - n)
+            n = 0
+            start = stop
+            break
         n += 1
         wait = False
         #print timestamp, offset, xeng_id, payload_len
         #print dout[256*packet_num:256*(packet_num+1)].shape
         #print len(data[16:])
         #print dout.shape, offset/4, (offset+PAYLOAD_LEN)/4, dout[offset/4:(offset+PAYLOAD_LEN)/4].shape, np.fromstring(data[16:], dtype='>i').shape
-        dout[offset/4:(offset+PAYLOAD_LEN)/4] = np.fromstring(data[16:], dtype='>i')
-        if  (timestamp != this_window):
-            print timestamp, this_window
-            stop = time.time()
-            print "%d packets received in %.2f seconds" % (n, stop - start)
-            n = 0
-            start = stop
-            break
+        dout[xeng_id, offset>>2:(offset+PAYLOAD_LEN)>>2] = np.fromstring(data[16:], dtype='>i')
 
-
-print "%d packets received in %.2f seconds" % (n, stop - start)
 
 print "Dumping packet to disk"
 with open("/tmp/packet.bin", "w") as fh:
-    fh.write(dout.tostring()) #This is native (probably little) endian!!
+    fh.write(dout.flatten().tostring()) #This is native (probably little) endian!!
 
