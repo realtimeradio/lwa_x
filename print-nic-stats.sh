7fc0ccc255f53635badac44a0a1f8189a434a6ac
jack <jack>
2018-11-04 21:51:37 +0000

    Add eth5 stats

--- print-nic-stats.sh
+++ print-nic-stats.sh
@@ -4,3 +4,7 @@ ethtool -S eth3 | grep rx_dropped
 ethtool -S eth3 | grep vport_dropped
 ethtool -S eth3 | grep rx_fifo_errors
 ethtool -S eth3 | grep rx_missed_errors
+ethtool -S eth5 | grep rx_dropped
+ethtool -S eth5 | grep vport_dropped
+ethtool -S eth5 | grep rx_fifo_errors
+ethtool -S eth5 | grep rx_missed_errors
