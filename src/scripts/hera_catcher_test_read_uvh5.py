f797272ee48630af2be897e602776007577de202
jack <jackh@berkeley>
2019-08-02 20:59:35 +0000

    include config file details

--- src/scripts/hera_catcher_test_read_uvh5.py
+++ src/scripts/hera_catcher_test_read_uvh5.py
@@ -1,6 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import h5py
+import redis
 import argparse
 
 parser = argparse.ArgumentParser(description='Read uvh5 files written out by the BDA catcher hashpipe pipeline and plot data',
@@ -10,5 +11,19 @@ args = parser.parse_args()
 
 fp = h5py.File(args.fname,'r')
 data = fp['Data']['visdata'].value
+
+# Check time array
+bdaconfig = '/tmp/config.txt'
+
+baselines = {}
+for n in range(4):
+    baselines[n] = []
+
+bdaconfig = np.loadtxt(configfile, dtype=np.int)
+for i,t in enumerate(bdaconfig[:,2]):
+    if (t==0): continue
+    n = int(np.log(t)/np.log(2))
+    if (n==4): n = 3
+    baselines[n].append((bdaconfig[i,0], bdaconfig[i,1]))
 
 
