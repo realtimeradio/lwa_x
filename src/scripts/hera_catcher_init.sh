6f1f45f6598d7728b2ce7765bfab2d6d0fef1fb4
jack <jackh@berkeley>
2019-10-07 22:44:21 +0000

    Log to Redis option

--- src/scripts/hera_catcher_init.sh
+++ src/scripts/hera_catcher_init.sh
@@ -21,46 +21,90 @@ function init() {
 
   if [ $USE_BDA -eq 1 ]
   then
-    netthread=hera_catcher_net_thread_bda
-    diskthread=hera_catcher_disk_thread_bda
-  else
-    netthread=hera_catcher_net_thread
-    diskthread=hera_catcher_disk_thread
+    echo "Using BDA threads"
+    echo taskset $mask \
+    hashpipe -p paper_gpu -I $instance \
+      -o BINDHOST=$bindhost \
+      -c $netcpu hera_catcher_net_thread_bda \
+      -c $outcpu hera_catcher_disk_thread_bda \
+      -c $autocpu hera_catcher_autocorr_thread
+
+    if [ $USE_REDIS -eq 1 ] 
+    then
+      echo "Using redis logger"
+      { taskset $mask \
+      hashpipe -p paper_gpu -I $instance \
+        -o BINDHOST=$bindhost \
+        -c $netcpu hera_catcher_net_thread_bda   \
+        -c $outcpu hera_catcher_disk_thread_bda  \
+        -c $autocpu hera_catcher_autocorr_thread \
+      < /dev/null 2>&3 1>~/catcher.out.$instance; } \
+      3>&1 1>&2 | tee ~/catcher.err.$instance | \
+      stdin_to_redis.py -l WARNING > /dev/null &
+    else
+      echo "*NOT* using redis logger"
+      taskset $mask \
+      hashpipe -p paper_gpu -I $instance \
+        -o BINDHOST=$bindhost \
+        -c $netcpu hera_catcher_net_thread_bda \
+        -c $outcpu hera_catcher_disk_thread_bda \
+        -c $autocpu hera_catcher_autocorr_thread \
+         < /dev/null \
+        1> ~/catcher.out.$instance \
+        2> ~/catcher.err.$instance &
+    fi
+
+  elif [ $USE_BDA -eq 0 ]
+  then
+    echo taskset $mask \
+    hashpipe -p paper_gpu -I $instance \
+      -o BINDHOST=$bindhost \
+      -c $netcpu hera_catcher_net_thread \
+      -c $outcpu hera_catcher_disk_thread
+
+    if [ $USE_REDIS -eq 1 ]
+    then
+      echo "Using redis logger"
+      { taskset $mask \
+      hashpipe -p paper_gpu -I $instance \
+        -o BINDHOST=$bindhost \
+        -c $netcpu hera_catcher_net_thread \
+        -c $outcpu hera_catcher_disk_thread \
+      < /dev/null 2>&3 1>~/catcher.out.$instance; } \
+      3>&1 1>&2 | tee ~/catcher.err.$instance | \
+      stdin_to_redis.py -l WARNING > /dev/null &
+    else
+      echo "*NOT* using redis logger"
+      taskset $mask \
+      hashpipe -p paper_gpu -I $instance \
+        -o BINDHOST=$bindhost \
+        -c $netcpu hera_catcher_net_thread \
+        -c $outcpu hera_catcher_disk_thread \
+         < /dev/null \
+        1> ~/catcher.out.$instance \
+        2> ~/catcher.err.$instance &
+    fi
   fi
-
-  echo "Using netthread: $netthread"
-  echo "Using diskthread: $diskthread"
-  
-  echo taskset $mask \
-  hashpipe -p paper_gpu -I $instance \
-    -o BINDHOST=$bindhost \
-    -c $netcpu $netthread \
-    -c $outcpu $diskthread
-
-  taskset $mask \
-  hashpipe -p paper_gpu -I $instance \
-    -o BINDHOST=$bindhost \
-    -c $netcpu $netthread \
-    -c $outcpu $diskthread \
-    -c $autocpu hera_catcher_autocorr_thread
-     < /dev/null \
-    1> ~/catcher.out.$instance \
-    2> ~/catcher.err.$instance &
 }
 
 # Default to not using BDA version
 USE_BDA=0
+USE_REDIS=0
 
 for arg in $@; do
   case $arg in
     -h)
       echo "Usage: $(basename $0) [-a] INSTANCE_ID [...]"
-      echo "   -a: Use baseline dependent averaging threads"
+      echo "  -a : Use baseline dependent averaging threads"
+      echo "  -r : Use redis logging (in addition to log files)"
       exit 0
     ;;
-
     -a)
       USE_BDA=1
+      shift
+    ;;
+    -r)
+      USE_REDIS=1
       shift
     ;;
   esac
@@ -69,7 +113,8 @@ done
 if [ -z "$1" ]
 then
   echo "Usage: $(basename $0) [-a] INSTANCE_ID [...]"
-  echo "   -a: Use baseline dependent averaging threads"
+  echo "  -a : Use baseline dependent averaging threads"
+  echo "  -r : Use redis logging (in addition to log files)"
   exit 1
 fi
 
